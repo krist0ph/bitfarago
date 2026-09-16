cb45ff4bf8055a8f26c6e936feb60eb005163f89
Kristoph <palotaikrili@gmail.com>
2026-09-16 13:02:20 +0200

    Updating the rest of the page components

---
+++ our-page/script.js
@@ -0,0 +1,17 @@
+clock = document.getElementById('clockId')
+
+function time() {
+    const today = new Date()
+    let hour = today.getHours()
+    let minute = today.getMinutes()
+    minute = checkTime(minute)
+    clock.innerHTML = hour + ":" + minute
+    setTimeout(time, 1000)
+}
+
+function checkTime(x) {
+    if (x < 10) {x = "0" + x}
+    return x
+}
+
+time()
